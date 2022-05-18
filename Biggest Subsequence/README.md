3fe1e0728a6a5e982f30531ff88dcd396c86b8f6
Gregor Kovač <kovac.gregi@gmail.com>
2022-05-18 20:25:42 +0200

    Added subsequence with the biggest sum

---
+++ Biggest Subsequence/README.md
@@ -0,0 +1,5 @@
+# Subsequence with the buggest sum
+
+**Description:** This algorithm finds the subsequence with the biggest sum in an array of numbers using the divide and conquer method. It also prints all the steps.
+
+**Usage:** Enter a number of numbers `N`, followed by `N` numbers. The program will output the algorithm flow in the form `[*array of numbers I'm working with*]: *biggest sum of a subsequence*`. The final sum can be seen in the last row of the output.
---
+++ Biggest Subsequence/biggestsubsequence.java
@@ -0,0 +1,80 @@
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class biggestsubsequence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        int[] t;
+
+        n = sc.nextInt();
+        t = new int[n];
+        for (int i = 0; i < n; i++)
+            t[i] = sc.nextInt();
+
+        maxSubsequenceSum(t);
+    }
+
+    public static int maxSubsequenceSum(int[] t) {
+        if (t.length <= 1) {
+            System.out.println("[" + t[0] + "]: " + t[0]);
+            return t[0];
+        }
+
+        int[] a, b;
+        int n = (int)Math.ceil((double)t.length/2);
+
+        a = new int[n];
+        b = new int[t.length - n];
+
+        for (int i = 0; i < t.length; i++) {
+            if (i < n)
+                a[i] = t[i];
+            else
+                b[i - n] = t[i];
+        }
+
+        int ai = a.length - 1, bi = 0;
+        int maxMidSum = a[ai] + b[bi];
+        int currMidSum = maxMidSum;
+
+        while (ai > 0) {
+            ai--;
+            currMidSum +=  a[ai];
+            if (currMidSum > maxMidSum)
+                maxMidSum = currMidSum;
+        }
+
+        currMidSum = maxMidSum;
+
+        while (bi < b.length - 1) {
+            bi++;
+            currMidSum += b[bi];
+            if (currMidSum > maxMidSum)
+                maxMidSum = currMidSum;
+        }
+        
+        int maxLeftSum = maxSubsequenceSum(a);
+
+        int ret = maxLeftSum;
+
+        int maxRightSum = maxSubsequenceSum(b);
+
+        if (maxMidSum > ret)
+            ret = maxMidSum;
+        if (maxRightSum > ret)
+            ret = maxRightSum;
+        
+        System.out.print("[");
+        for (int i = 0; i < t.length; i++) {
+            if (i != 0)
+                System.out.print(", ");
+            System.out.print(t[i]);
+        }
+        System.out.println("]: " + ret);
+
+        return ret;
+    }
+}
